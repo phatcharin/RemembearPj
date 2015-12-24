753b0f8f616fff992bc6342c7dc6980be9fe05a9
vamnoize <snowlandvamvam@gmail.com>
2015-12-25 00:01:36 +0700

    Menu bar

---
+++ ParseStarterProject/src/main/java/com/parse/starter/GameHomeActivity.java
@@ -0,0 +1,22 @@
+package com.parse.starter;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Created by vamnoize on 24/12/2558.
+ */
+public class GameHomeActivity extends ActionBarActivity {
+    private Toolbar mToolbar;
+    private Toolbar mToolbarBottom;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gamehome);
+
+    }
+
+
+}
